d8df3719119e2b9c7de8c3d1a445bdfe7082f647
CarminePernolino <c.pernolino@studenti.unimol.it>
2024-03-15 10:28:45 +0100

    Creazione funzione di stampa Board in Menu

--- src/main/java/org/example/gui/Menu.java
+++ src/main/java/org/example/gui/Menu.java
@@ -1,4 +1,34 @@
 package org.example.gui;
 
+import org.example.backend.Board;
+
 public class Menu {
+    Board board;
+
+    public Menu() {
+        this.board = new Board();
+        this.printBoard();
+    }
+
+    private void printBoard() {
+        int contCell = 1;
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print("[" + "-" + "]");
+            }
+
+            if(i == 1)
+                System.out.print("     ->     ");
+            else
+                System.out.print("            ");
+
+            for(int j = 0; j < 3; j++) {
+                System.out.print("[" + contCell + "]");
+                contCell++;
+            }
+
+            System.out.print("\n");
+        }
+    }
 }
