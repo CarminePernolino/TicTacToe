d48c7afc802c45e6cc6e4bbbb1d985cacbd0a5f6
CarminePernolino <c.pernolino@studenti.unimol.it>
2024-03-15 10:23:04 +0100

    Creazione prima funzione in Board

--- src/main/java/org/example/backend/Board.java
+++ src/main/java/org/example/backend/Board.java
@@ -1,4 +1,17 @@
 package org.example.backend;
 
 public class Board {
+    private int board[][] = new int[3][3];
+
+    public Board() {
+        this.createBoard();
+    }
+
+    private void createBoard() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                board[i][j] = -1;
+            }
+        }
+    }
 }
